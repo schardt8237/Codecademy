4509c4cc6adab9ef97c488d6681336562f9370f7
Michael <52144224+schardt8237@users.noreply.github.com>
2019-08-20 22:55:39 -0400

    Add files via upload

---
+++ Data Science Career Path/Unit 3: SQL Intermediate/cryptocurrency_exchange.sql
@@ -0,0 +1,11 @@
+SELECT * FROM transactions;
+SELECT SUM(money_in) FROM transactions;
+SELECT SUM(money_out) FROM transactions;
+SELECT COUNT(money_in) FROM transactions;
+SELECT COUNT(money_in) FROM transactions WHERE currency = "BIT";
+SELECT MAX(money_in) FROM transactions;
+SELECT MAX(money_out) FROM transactions;
+SELECT AVG(money_in) FROM transactions WHERE currency = "ETH";
+SELECT date, AVG(money_in), AVG(money_in) FROM transactions GROUP BY date;
+SELECT date, ROUND(AVG(money_in),2), ROUND(AVG(money_in),2) FROM transactions GROUP BY date;
+SELECT date, AVG(money_in) AS "Avg Buy", AVG(money_in) AS "Avg Sell" FROM transactions GROUP BY date;
