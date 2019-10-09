d4859eff627283b7005a611e7a53f3c7cc3cf832
Michael <52144224+schardt8237@users.noreply.github.com>
2019-10-09 00:42:35 -0400

    Create initials.cpp

---
+++ Learn C++/Unit 1: Hello World/initials.cpp
@@ -0,0 +1,15 @@
+// I am a distant relative to Mark Twain
+
+#include <iostream>
+
+int main() {
+
+  // Your program lives here:
+  std::cout << "M   M JJJJJ  SSS\n";
+  std::cout << "MM MM   J   S   S\n";
+  std::cout << "MM MM   J   S\n";
+  std::cout << "M M M   J    SSS\n";
+  std::cout << "M   M J J       S\n";
+  std::cout << "M   M J J   S   S\n";
+  std::cout << "M   M  JJ    SSS\n";
+}
